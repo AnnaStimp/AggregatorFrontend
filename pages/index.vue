2237893058625f7a5e005a2672c8395622052b3b
AnnaStimp <strashenkoanna@mail.ru>
2021-11-21 22:30:59 +0300

    need change slader

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="homePage">
     <div class="homePage__sliderWrap">
-      <div class="homePage__sliderWrap__slide">
-        <img src="@/assets/images/HomePage/banner.webp" alt="">
-        <div class="homePage__sliderWrap__slide__inf">
-          <h3>Yves Saint Laurent до −35%</h3>
-          <p>только 11 и 12 сентября</p>
-          <div class="homePage__sliderWrap__slide__inf__btn">
-            <p>Узнать подробнее</p>
-            <RightArrow />
+      <div class="homePage__sliderWrap__slider">
+        <div
+          class="homePage__sliderWrap__slider__slide"
+          v-for="i in slides"
+          :key="i"
+        >
+          <img src="@/assets/images/HomePage/banner.webp" alt="">
+          <div class="homePage__sliderWrap__slider__slide__inf">
+            <h3>Yves Saint Laurent до −35%</h3>
+            <p>только 11 и 12 сентября</p>
+            <div class="homePage__sliderWrap__slider__slide__inf__btn">
+              <p>Узнать подробнее</p>
+              <RightArrow />
+            </div>
           </div>
         </div>
       </div>
       <div class="homePage__sliderWrap__buttons">
-        <div class="homePage__sliderWrap__buttons__btn"></div>
-        <div class="homePage__sliderWrap__buttons__btn"></div>
-        <div class="homePage__sliderWrap__buttons__btn"></div>
+        <div
+          v-for="(i, index) in slides"
+          :key="index"
+          class="homePage__sliderWrap__buttons__btn"
+          :class="{active: index}"
+          @click="slide('0')"
+        ></div>
       </div>
     </div>
   </div>
@@ -30,6 +40,17 @@ export default {
   name: 'HomePage',
   components: {
     RightArrow
+  },
+  data () {
+    return {
+      slides: [1, 2, 3, 4]
+    }
+  },
+  methods: {
+    slide (position) {
+      document.querySelector('.homePage__sliderWrap__slider').style.transform = `translateX(calc(${position}))`
+      console.log(document.querySelector('.homePage__sliderWrap__slider').style.transform = `translateX(calc(${position}))`)
+    }
   }
 }
 </script>
